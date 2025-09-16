0483a3752ddb987f9a7b1eb14bfbe918e93eb5de
joao oliveira <xliveirvwrld@gmail.com>
2025-09-16 14:04:49 -0300

    refactor: adjust imports

--- orders-service/src/main/java/br/com/joao/orders_service/repository/OrderRepository.java
+++ orders-service/src/main/java/br/com/joao/orders_service/repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package br.com.joao.orders_service.repository;
 
 import br.com.joao.orders_service.domain.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByUserEmailIgnoreCase(String userEmail, Pageable pageable);
 }
